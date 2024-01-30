ea489cb78bba2e6cd7b3a9a75a3d8955946c4940
Adil Shaikh <156773574+sadil15@users.noreply.github.com>
2024-01-30 10:08:32 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const accountId = 45213;
+let accountPassword = "12345";
+let accountState;
+let temp = null;
+
+console.table([accountId, accountPassword, accountState]);
+
+console.log(accountId, accountPassword);
+
+console.log(typeof(temp));
