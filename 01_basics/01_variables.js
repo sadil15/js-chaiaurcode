257f1263f2d8bac68e9bb724b317a353120d3f8f
Adil Shaikh <156773574+sadil15@users.noreply.github.com>
2024-01-30 12:43:29 +0000

    types of memory

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -10,6 +10,18 @@ const anotherId = Symbol("123");
 
 const bigNum = 6551648494516216518n;
 
+const heroes = ["spiderman", "ironman", "batman", "superman"];
+
+const student = {
+    name: "Adil",
+    age: 19,
+    cgpa: 9.8
+};
+
+const func = () => {
+    console.log("Hello World");
+};
+
 console.table([accountId, accountPassword, accountState]);
 
 console.log(accountId, accountPassword);
@@ -21,3 +33,9 @@ console.log(id == anotherId);
 console.log(id === anotherId);
 
 console.log(typeof(bigNum));
+
+console.log(typeof(heroes));
+console.log(typeof(student));
+
+func();
+console.log(typeof(func));
---
+++ 01_basics/memory.js
@@ -0,0 +1,20 @@
+let myYtName = "adildotcom";
+
+let anotherName = myYtName;
+console.log(anotherName);
+anotherName = "chaiaurcode";
+
+console.log(myYtName);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
+};
+
+let userTwo = userOne;
+
+userTwo.email = "adil@gmail.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
