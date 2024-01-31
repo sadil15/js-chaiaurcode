1f8559921a2c33bb4db87a8887c5f7bda9104c50
Adil Shaikh <156773574+sadil15@users.noreply.github.com>
2024-01-31 08:19:02 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,64 @@
+// let arr = new Array(1, "adil", 5, "junaid");
+// console.log(arr);
+
+// arr.push(6);
+// arr.push(12);
+// console.log(arr);
+
+// arr.pop();
+// console.log(arr);
+
+// arr.unshift(99);
+// console.log(arr);
+
+// arr.shift();
+// console.log(arr);
+
+// console.log(arr.includes("adil"));
+// console.log(arr.indexOf(5));
+
+// const newArr = arr.join();
+// console.log(arr);
+// console.log(typeof(newArr));
+
+// console.log(arr); // original array
+// let sliceArr = arr.slice(1, 3);
+// console.log(sliceArr);
+// console.log(arr);
+
+// let spliceArr = arr.splice(1, 3);
+// console.log(spliceArr);
+// console.log(arr);
+
+
+
+const marvel_heroes = ["thor", "ironman", "spiderman"];
+
+const dc_heroes = ["flash", "batman", "superman"];
+
+// marvel_heroes.push(dc_heroes);
+
+// console.log(marvel_heroes);
+// console.log(marvel_heroes[3][1]);
+
+// const allHeroes = marvel_heroes.concat(dc_heroes);
+// console.log(allHeroes);
+
+// const allNewHeroes = [...marvel_heroes, ...dc_heroes];
+
+// console.log(allNewHeroes);
+
+// const anotherArr = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+
+// let realArr = anotherArr.flat(1);
+// console.log(realArr);
+
+
+// console.log(Array.from("Adil"));
+
+// let score1 = 100;
+// let score2 = 200;
+// let score3 = 300;
+
+// let allScore = Array.of(score1, score2, score3);
+// console.log(allScore);
